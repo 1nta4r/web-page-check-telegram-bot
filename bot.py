2be30d4d7bc1134b49c8a425871ae0cc88f79c98
1nta4r <1nta4r@gmail.com>
2024-03-02 16:32:21 +0300

    add logging, bot config and urls file

--- bot.py
+++ bot.py
@@ -1,47 +1,92 @@
+import configparser
 import requests
 import hashlib
+import logging
 import time
 import os
 
 
-BOT_TOKEN = ''
-CHAT_ID = ''
+class WebCheckBot():
+
+    def __init__(self) -> None:
+        self.bot_token = ''
+        self.chat_id = ''
+        self.timeout = 60
+        self.urls_list = []
 
 
-def report_changes(url):
-    html_response = requests.get(url).text
-    html_response_hash = str(hashlib.sha3_256(html_response.encode()).hexdigest())
-    file_name = ''.join(x for x in url if x.isalpha()) + ".txt"
-    if os.path.exists(file_name):
-        cache_file = open(file_name, "r")
-        html_cache = cache_file.read()
-        if html_response_hash != html_cache:
+    def set_urls_list_from_file(self, path_to_file: str):
+        try:
+            if os.path.isfile(path_to_file):
+                with open(path_to_file) as urls_file:
+                    urls_list = urls_file.readlines()
+                self.urls_list = [url.strip() for url in urls_list]
+        except:
+            logging.warn('Cann\'t read urls file')
+            pass
+    
+    
+    def set_timeout(self, new_timeout: int):
+        if 86400 > new_timeout > 9:
+            self.timeout = new_timeout 
+        else:
+            self.timeout = 60
+            logging.info('Timeout is greater or less than the limit. The default value is set: {}'.format(60))
+
+
+    def set_params_from_config(self, path_to_config: str):
+        config = configparser.ConfigParser()
+        config.read(path_to_config)
+        logging.info('Config file is loaded...')
+        try:
+            self.bot_token = config['BOT']['token']
+            self.chat_id = config['BOT']['chat_id']
+            self.timeout = int(config['MONITORING']['timeout']) if 'timeout' in config['MONITORING'] else 60
+            self.set_urls_list_from_file(config['MONITORING']['urls_list_path'])
+            logging.info('Params are set...')
+            if self.bot_token == "''":
+                logging.error('Bot token is empty!')
+                raise ValueError('Bot token is empty!')
+            elif self.chat_id == "''":
+                logging.error('Chat id is empty!')
+                raise ValueError('Chat id is empty!')      
+        except:
+            logging.warn('Some parameters are missing in the config file (token, chat_id, timeout)')
+            exit(-1)
+
+
+    def scan_urls(self):
+        for url in self.urls_list:
+            self.report_changes(url)
+            time.sleep(0.5)
+
+
+    def report_changes(self, url: str):
+        html_response = requests.get(url).text
+        html_response_hash = str(hashlib.sha3_256(html_response.encode()).hexdigest())
+        file_name = ''.join(x for x in url if x.isalpha()) + ".txt"
+        if os.path.exists(file_name):
+            cache_file = open(file_name, "r")
+            html_cache = cache_file.read()
+            if html_response_hash != html_cache:
+                cache_file = open(file_name, "w")
+                cache_file.write(html_response_hash)
+                self.send_to_bot("Changes detected at url: " + url)
+        else:
+            print("No cache file for " + url + " found, creating one...")
             cache_file = open(file_name, "w")
             cache_file.write(html_response_hash)
-            send_to_bot("Changes detected at url: " + url)
-    else:
-        print("No cache file for " + url + " found, creating one...")
-        cache_file = open(file_name, "w")
-        cache_file.write(html_response_hash)
 
 
-def scan_urls():
-    with open("urls.txt") as urls_file:
-        urls_list = urls_file.readlines()
-    urls_list = [x.strip() for x in urls_list]
-    for url in urls_list:
-        report_changes(url)
-        time.sleep(1)
+    def send_to_bot(self, bot_message):
+        send_text = 'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatID}\
+            &parse_mode=Markdown&text={bot_message}'.format(bot_token=self.bot_token, chatID=self.chat_id, bot_message=bot_message)
+        response = requests.get(send_text)
+        return response.json()
 
 
-def send_to_bot(bot_message):
-    send_text = 'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatID}\
-        &parse_mode=Markdown&text={bot_message}'.format(bot_token=BOT_TOKEN, chatID=CHAT_ID, bot_message=bot_message)
-    response = requests.get(send_text)
-    return response.json()
-
-
-def run():
-    while True:
-        scan_urls()
-        time.sleep(10)
+    def run(self):
+        logging.info('Bot is running...')
+        while True:
+            self.scan_urls()
+            time.sleep(self.timeout)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import logging
 import bot
 
-
 if __name__ == '__main__':
-    bot.run()
+    logging.basicConfig(filename='WebCheckBot.log', level=logging.INFO)
+    bot_instance = bot.WebCheckBot()
+    bot_instance.set_params_from_config('bot_config.ini')
+    bot_instance.run()
 
 
 
